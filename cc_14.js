8d7855b52b2f9785a022ba362306ff9588168ad1
mvivy3 <meredithyen@usf.edu>
2025-03-09 23:57:43 -0400

    Task 5 - Inline Editing for Support Tickets

--- cc_14.js
+++ cc_14.js
@@ -66,3 +66,40 @@ ticket.appendChild(priorityState);
 ticket.appendChild(resolveButton);
 ticket.appendChild(editButton);
 ticketContainer.appendChild(ticket);
+
+ // TASK 5: Additional Challenge – Inline Editing of Support Tickets
+ ticket.addEventListener(`dblclick`, function(){
+    const nameInput = document.createElement(`input`);
+    nameInput.setAttribute(`type`,`text`);
+    nameInput.value = nameHeading.textContent;
+
+    const issueInput = document.createElement(`input`);
+    issueInput.setAttribute(`type`,`text`);
+    issueInput.value = issueText.textContent;
+
+    const priorityInput = document.createElement(`input`);
+    priorityInput.setAttribute(`type`,`text`);
+    priorityInput.value = priorityState.textContent.split(": ")[1];
+    const saveButton = document.createElement(`button`);
+    saveButton.textContent = `Save`;
+  
+    ticket.innerHTML = ``;
+    ticket.appendChild(nameInput);
+    ticket.appendChild(issueInput);
+    ticket.appendChild(priorityInput);
+    ticket.appendChild(saveButton);
+
+    saveButton.addEventListener(`click`, function(){ 
+    
+        nameHeading.textContent = nameInput.value;
+        issueText.textContent = issueInput.value;
+        priorityState.textContent = `Priority : ${priorityInput.value}`;
+        priorityState.setAttribute(`class`, priorityInput.value.toLowerCase());
+        
+        ticket.innerHTML = ``;
+        ticket.appendChild(nameHeading);
+        ticket.appendChild(issueText);
+        ticket.appendChild(priorityState);
+        ticket.appendChild(resolveButton);
+        ticket.appendChild(editButton);
+    })
